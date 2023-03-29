60d1389d3d1d573c6944d2cfcf5c6287f1d82377
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-29 09:38:32 +0200

    coverage in its own module

---
+++ coverage.lua
@@ -0,0 +1,20 @@
+local ie = ...
+if ie then
+    print("[mtt] coverage via luacov enabled")
+
+    mtt.old_io = io
+    require = ie.require
+    io = ie.io
+    mtt.luacov_runner = require("luacov.runner")
+
+    local include_paths = {}
+    for modname in pairs(mtt.get_enabled_mods()) do
+        table.insert(include_paths, minetest.get_modpath(modname))
+    end
+
+    mtt.luacov_runner.init({
+        include = include_paths,
+        reporter = "lcov",
+        reportfile = "lcov.info"
+    })
+end
--- init.lua
+++ init.lua
@@ -30,23 +30,7 @@ if mtt.enabled then
     if mtt.enable_coverage then
         -- enable coverage
         local ie = minetest.request_insecure_environment and minetest.request_insecure_environment()
-        if ie then
-            mtt.old_io = io
-            require = ie.require
-            io = ie.io
-            mtt.luacov_runner = require("luacov.runner")
-
-            local include_paths = {}
-            for modname in pairs(mtt.get_enabled_mods()) do
-                table.insert(include_paths, minetest.get_modpath(modname))
-            end
-
-            mtt.luacov_runner.init({
-                include = include_paths,
-                reporter = "lcov",
-                reportfile = "lcov.info"
-            })
-        end
+        loadfile(MP .. "/coverage.lua")(ie)
     end
 
     -- player api override
