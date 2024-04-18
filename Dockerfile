85f526b5f0db549b6c67d1ef0437306411695c0e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-04-18 10:51:58 +0200

    args test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM registry.gitlab.com/minetest/minetest/server:5.6.0
+FROM registry.gitlab.com/minetest/minetest/server:5.6.1
 USER root
 
 RUN apk add --no-cache git &&\
     mkdir -p /root/.minetest/worlds/world/worldmods/ &&\
     git clone https://github.com/BuckarooBanzay/mtt /root/.minetest/worlds/world/worldmods/mtt
 
-ENTRYPOINT minetestserver --config /minetest.conf
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT /entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ln -s /root/.minetest/worlds/world/worldmods/test_mod /github/workspace
+
+cat <<EOF > /minetest.conf
+default_game = minetest_game
+mg_name = singlenode
+mtt_filter = mtt
+mtt_enable = true
+mtt_enable_benchmarks = true
+mtt_export_nodenames = true
+mtt_enable_selftest = true
+EOF
+
+cat <<EOF > /root/.minetest/worlds/world/world.mt
+enable_damage = false
+creative_mode = true
+mod_storage_backend = sqlite3
+auth_backend = sqlite3
+player_backend = dummy
+backend = dummy
+gameid = minetest
+world_name = mtt
+EOF
+
+minetestserver --config /minetest.conf
