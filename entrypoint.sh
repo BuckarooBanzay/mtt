001ba3c6d44947b909ad068053a900efbcb91030
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-04-18 11:18:32 +0200

    config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
-export
 
-
+# create directory skeleton
 mkdir -p ${HOME}/.minetest/worlds/world/worldmods/
-cd ${HOME}/.minetest/worlds/world/worldmods/
 
+# clone dependencies
+cd ${HOME}/.minetest/worlds/world/worldmods/
 for dep in ${INPUT_DEPENDENCIES}
 do
     git clone $dep
 done
 
-
+# create link to current mod
 ln -s /github/workspace ${HOME}/.minetest/worlds/world/worldmods/${INPUT_MODNAME}
 
+# assemble minetest.conf
 cat <<EOF > /minetest.conf
 default_game = minetest_game
 mg_name = singlenode
-mtt_filter = mtt
+mtt_filter = ${INPUT_MODNAME}
 mtt_enable = true
-mtt_enable_benchmarks = true
-mtt_export_nodenames = true
-mtt_enable_selftest = true
 EOF
+cat ${INPUT_ADDITIONAL_CONFIG} >> /minetest.conf
 
+# simple world.mt
 cat <<EOF > ${HOME}/.minetest/worlds/world/world.mt
 enable_damage = false
 creative_mode = true
@@ -34,4 +34,5 @@ gameid = minetest
 world_name = mtt
 EOF
 
+# start the engine
 minetestserver --config /minetest.conf
