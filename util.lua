0d0b3608c694e09a36d9d711c9efb4bf1b602d89
OgelGames <olliverdc28@gmail.com>
2026-09-10 11:54:17 +1000

    increase emerge area timeout

--- util.lua
+++ util.lua
@@ -1,13 +1,15 @@
 
 function mtt.emerge_area(pos1, pos2)
-    mtt.register("emerge_area " .. minetest.pos_to_string(pos1) .. " -> " .. minetest.pos_to_string(pos2),
-    function(callback)
-        minetest.emerge_area(pos1, pos2, function(_, _, calls_remaining)
-            if calls_remaining == 0 then
-                callback()
-            end
-        end)
-    end)
+    mtt.register("emerge_area " .. minetest.pos_to_string(pos1) .. " -> " .. minetest.pos_to_string(pos2), {
+		func = function(callback)
+			minetest.emerge_area(pos1, pos2, function(_, _, calls_remaining)
+				if calls_remaining == 0 then
+					callback()
+				end
+			end)
+		end,
+		timout = 30,
+	})
 end
 
 -- current testing area offset
